44de103e4ff9df1fdfa7c94392ad7339653a412b
Arjun Gautam <arjuncodes@Arjuns-MacBook-Pro.local>
2022-02-11 11:44:00 +0545

    reformat

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import datetime
 import os
 from pathlib import Path
 
-currentDate=datetime.datetime.today()
+currentDate = datetime.datetime.today()
 
-log_path=os.path.join("/Users/arjuncodes/Desktop/"+currentDate.strftime("%Y%m%d")+"/data.py")
+log_path = os.path.join("/Users/arjuncodes/Desktop/" + currentDate.strftime("%Y%m%d") + "/data.py")
 
 if os.path.exists(log_path):
-    log_path=log_path
+    log_path = log_path
     print("File already exists")
 else:
-    log_path=Path("/Users/arjuncodes/Desktop/"+currentDate.strftime("%Y%m%d")+"/data.py")
-    os.makedirs(os.path.dirname(log_path),exist_ok=True)
-    f=open(log_path,"a+")
+    log_path = Path("/Users/arjuncodes/Desktop/" + currentDate.strftime("%Y%m%d") + "/data.py")
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    f = open(log_path, "a+")
 
 # if os.path.exists(log_path):
 #     log_path=log_path
